23b0fef29feedad5f331c914686bb993e71deaa6
Sjpace1999 <sjpace1999@gmail.com>
2021-10-26 16:14:08 -0400

    Jump added

--- 2DWeek/Assets/Scripts/Player/PlayerController.cs
+++ 2DWeek/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidBody2d;
+
+    public float runSpeed = 5f;
+    public float jumpSpeed = 200f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput * runSpeed * Time.deltaTime, 0);
+
+        rigidBody2d.AddForce(direction);
+    }
+
+    void Jump()
+    {
+        rigidBody2d.velocity = new Vector2(rigidBody2d.velocity.x, jumpSpeed);
     }
 }
